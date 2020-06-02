3daf66aa89d7f494dafc6b1a0425fa4d4fa93d34
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-06-02 01:47:30 +0100

    Level-2

--- src/App.js
+++ src/App.js
@@ -6,19 +6,35 @@ import CountryCard from './CountryCard';
 function App() {
   const [country, setCountry] = useState(data)
 
-  const handleInputChange = (e) => {
+  const handleInputChange1 = (e) => {
       const searchedCountry = e.target.value.toLowerCase();
       const filteredCountry = data.filter(item => item.name.toLowerCase().includes(searchedCountry))
       setCountry(filteredCountry)
   }
 
+  const handleInputChange2 = (e) => {
+    const searchedCity = e.target.value.toLowerCase();
+    const filteredCountryByCity = data.filter(item => item.capital.toLowerCase().includes(searchedCity))
+    setCountry(filteredCountryByCity)
+}
+
   return (
     <div className="App">
       <div>
         <input type="text" 
-             onChange={handleInputChange}
+             onChange={handleInputChange1}
              placeholder='Search for country' 
-             style={{border: "2px solid blue", margin: '20px 20px'}}/>
+             style={{border: "2px solid blue", 
+                     margin: '20px 20px', 
+                    width: '150px',
+                    padding: '10px'}}/>
+        <input type="text" 
+             onChange={handleInputChange2}
+             placeholder='Search for capital city' 
+             style={{border: "2px solid blue", 
+                     margin: '20px 20px', 
+                    width: '150px',
+                    padding: '10px'}}/>
       </div>
       <div className='app-body'>
         {country.map((country) => (
